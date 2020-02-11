24519573b6d6f34fdda2cb8733590589b50584e4
showe93 <showe1364@gmail.com>
2020-02-11 10:44:42 -0500

    bank

--- src/comp152/BankAccount.java
+++ src/comp152/BankAccount.java
@@ -3,13 +3,30 @@ package comp152;
 public class BankAccount {
     private double balance;
     private float interestRate;
+    private int accountID;
+    private static int nextID = 1000;
+
+    public BankAccount(){
+        interestRate=0.02f;
+        accountID=nextID;
+        nextID++;
+    }
+    public BankAccount(double initialBalance, float initialRate){
+        balance = initialBalance;
+        interestRate = initialRate;
+        accountID=nextID;
+        nextID=nextID+1;
+
+    }
 
     public double addInterest(){
         balance = (balance*interestRate) + balance;
         return balance;
     }
     public void deposit(double amount) {
-        balance = balance + amount;
+        if (amount > 0.0){
+            balance = balance + amount;
+        }
     }
     public boolean withdraw(double amount) {
         if(amount<=balance) {
@@ -21,6 +38,8 @@ public class BankAccount {
     public double checkBalance(){
         return balance;
     }
+
+
 
 }
 
--- src/comp152/Main.java
+++ src/comp152/Main.java
@@ -2,6 +2,17 @@ package comp152;
 
 public class Main {
     public static void main(String[] args){
-        
+        var myAccount = new BankAccount();
+        var yourAccount = new BankAccount(10000, .05f);
+        myAccount.deposit(1000);
+        myAccount.addInterest();
+        yourAccount.addInterest();
+        System.out.println("My account has " + myAccount.checkBalance());
+        System.out.println("Your account has " + yourAccount.checkBalance());
+        var succeeded = myAccount.withdraw(2000);
+        if(succeeded)
+            System.out.println("Manages to withdraw 2000");
+        else
+            System.out.println("Tough Luck buddy, you only have $" + myAccount.checkBalance()+"...dam penguins");
     }
 }
